9469b6ca409f70ae2d506c8a69b88708bb63313a
Laura Hamilton, Mick Sexton and Saul Artiga <laura+mick+saul@>
2017-08-01 12:19:10 -0700

    create 1st draft of a node project template

---
+++ install-dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+npm install gulp --save-dev
+bower install jquery --save
+bower install bootstrap --save
+npm install bower-files --save-dev
+npm install browser-sync --save-dev
+npm install browserify --save-dev
+npm install vinyl-source-stream --save-dev
+npm install gulp-concat --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-util --save-dev
+npm install del --save-dev
+npm install jshint --save-dev
+npm install gulp-jshint --save-dev
+npm install gulp-sass gulp-sourcemaps --save-dev
