c5b9a84e54d635654f0b6cfdfeea84f847fdc891
slbrtg <slbrtg@gmail.com>
2017-08-01 12:27:52 -0700

    Update kickstart-node.sh

--- kickstart-node.sh
+++ kickstart-node.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+echo "______________________________________"
+echo "**** npm init ****"
+echo "______________________________________"
 npm init
+echo "______________________________________"
+echo "**** bower init ****"
+echo "______________________________________"
 bower init
+echo "______________________________________"
+echo "**** installing dependencies ****"
+echo "______________________________________"
 npm install gulp --save-dev
 bower install jquery --save
 bower install bootstrap --save
@@ -15,3 +24,6 @@ npm install del --save-dev
 npm install jshint --save-dev
 npm install gulp-jshint --save-dev
 npm install gulp-sass gulp-sourcemaps --save-dev
+echo "______________________________________"
+echo "**** script complete ****"
+echo "______________________________________"
